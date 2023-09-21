e2b5afb34e991dfb8a4fe371f05b979ff1ecc7b6
Rafal Podkanski <rafal.podkanski@gmail.com>
2023-09-21 16:58:52 +0200

    hmm

--- zadanie.py
+++ zadanie.py
@@ -1,1 +1,14 @@
-specjalne pozdrowienia.
+print("Lista zakupów")
+shopping_list = {
+    "Piekarnia": ["Chleb", "Pączek", "Bułki"],
+    "Warzywniak": ['Marchew', 'Seler', 'Rukola']
+}
+for sklep, rzeczy in shopping_list.items():
+  print(f"Idę do {sklep} i kupuję tam: {rzeczy}.")
+for rzeczy in shopping_list.values():
+  print(f"W sumie kupuję {(len(rzeczy)*(len(shopping_list)))} produktów.")
+  break
+
+print('commit1')
+
+print("specjalne pozdrowienia dla mentora")
